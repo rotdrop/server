2502773227f1bace25894a273d078938ab1a2aae
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-10-13 18:56:50 +0200

    Allow groupOfMembers as well.

--- apps/user_ldap/lib/Wizard.php
+++ apps/user_ldap/lib/Wizard.php
@@ -432,7 +432,7 @@ class Wizard extends LDAPUtility {
 	 * @throws \Exception
 	 */
 	public function fetchGroups($dbKey, $confKey) {
-		$obclasses = ['posixGroup', 'group', 'zimbraDistributionList', 'groupOfNames', 'groupOfUniqueNames'];
+		$obclasses = ['posixGroup', 'group', 'zimbraDistributionList', 'groupOfNames', 'groupOfUniqueNames', 'groupOfMembers'];
 
 		$filterParts = [];
 		foreach ($obclasses as $obclass) {
@@ -512,7 +512,7 @@ class Wizard extends LDAPUtility {
 			throw new \Exception('Could not connect to LDAP');
 		}
 
-		$obclasses = ['groupOfNames', 'groupOfUniqueNames', 'group', 'posixGroup', '*'];
+		$obclasses = ['groupOfNames', 'groupOfUniqueNames', 'group', 'posixGroup', 'groupOfMembers', '*'];
 		$this->determineFeature($obclasses,
 								'objectclass',
 								'ldap_groupfilter_objectclass',
