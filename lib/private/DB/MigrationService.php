921a97e478c9182356d4262bda49d3d1743f552e
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-10-23 02:04:27 +0200

    The primary index name just cannot be too long if left at default ...

--- lib/private/DB/MigrationService.php
+++ lib/private/DB/MigrationService.php
@@ -560,9 +560,6 @@ class MigrationService {
 				if (!$isUsingDefaultName && \strlen($indexName) > 30) {
 					throw new \InvalidArgumentException('Primary index name  on "'  . $table->getName() . '" is too long.');
 				}
-				if ($isUsingDefaultName && \strlen($table->getName()) - $prefixLength >= 23) {
-					throw new \InvalidArgumentException('Primary index name  on "'  . $table->getName() . '" is too long.');
-				}
 			}
 		}
 
