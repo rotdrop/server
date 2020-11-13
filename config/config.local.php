1b943e7c1d354ae8a194d600ffdd84b82513051f
Claus-Justus Heine <himself@claus-justus-heine.de>
2020-11-13 13:31:31 +0100

    Get local config out of the way of git rebase.

---
+++ config/config.local.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG_LOCAL = [
+  'instanceid' => 'ocbnl45ljkp9',
+  'passwordsalt' => 'ilFgffxjhcNnRCf2cW5OdvYAQG/qo2',
+  'secret' => 'M3rmsy/gIarCn2Z/Nb/epZHdb57NgrOqlmTKQ5iO7WAp+PIM',
+  'trusted_domains' => 
+  array (
+    0 => 'anaxagoras.home.claus-justus-heine.de',
+  ),
+  'datadirectory' => '/var/www/localhost/data/nextcloud-git',
+  'dbtype' => 'mysql',
+  'version' => '20.0.1.1',
+  'overwrite.cli.url' => 'https://anaxagoras.home.claus-justus-heine.de/nextcloud-git',
+  'dbname' => 'nextcloud_git',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'Di1tPw!?',
+  'installed' => true,
+  'ldapIgnoreNamingRules' => false,
+  'ldapProviderFactory' => 'OCA\\User_LDAP\\LDAPProviderFactory',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  /**
+   * Loglevel to start logging at. Valid values are: 0 = Debug, 1 = Info, 2 =
+   * Warning, 3 = Error, and 4 = Fatal. The default value is Warning.
+   *
+   * Defaults to ``2``
+   */
+  'loglevel' => 1,
+];
