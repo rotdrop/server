e38fb815f4956e7160d800f6a6b7d4cf599d34af
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-02-17 11:18:57 +0100

    Controller, JSON-Responder: keep the CSP-policy if set by DataResponse.

--- lib/public/AppFramework/Controller.php
+++ lib/public/AppFramework/Controller.php
@@ -86,6 +86,9 @@ abstract class Controller {
 					}
 					$response->setHeaders(array_merge($dataHeaders, $headers));
 
+					// also keep the CSP headers
+					$response->setContentSecurityPolicy($data->getContentSecurityPolicy());
+
 					if ($data->getETag() !== null) {
 						$response->setETag($data->getETag());
 					}
